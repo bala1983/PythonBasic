4984c1ba3967861fe6468632fd47f48d6407f781
Oleksandr <bala1983@ukr.net>
2023-02-19 16:50:30 +0200

    DZ10

---
+++ DZ10.py
@@ -0,0 +1,7 @@
+list1 = [1, 2, 0, 6, 5, 6, 14, 8, 9, 10]
+counter = 0
+
+for num in range(1, len(list1)-1):  # числа
+    if list1[num] > list1[num-1] and list1[num] > list1[num+1]:
+        counter += 1
+print("Кількість елементів ", counter)
--- DZ7.py
+++ DZ7.py
@@ -1,2 +1,7 @@
-text = input("Введіть рядок:")
-print("Кількість слів: ", len(text.split()))
+#text = input("Введіть рядок:")
+#print("Кількість слів: ", len(text.split()))
+list = [1, 2, 3]
+
+a = list[1 -1]
+print(a)
+
