6c58eafb99dbbb9bad2eca3c214e165364888cff
Oleksandr <bala1983@ukr.net>
2023-02-19 16:58:40 +0200

    DZ10

--- DZ7.py
+++ DZ7.py
@@ -1,7 +1,2 @@
-#text = input("Введіть рядок:")
-#print("Кількість слів: ", len(text.split()))
-list = [1, 2, 3]
-
-a = list[1 -1]
-print(a)
-
+text = input("Введіть рядок:")
+print("Кількість слів: ", len(text.split()))
