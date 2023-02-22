2fd51917dab526a35e50f6ac39543883168258ea
Oleksandr <bala1983@ukr.net>
2023-02-22 22:32:08 +0200

    DZ13

--- DZ13.py
+++ DZ13.py
@@ -1,5 +1,14 @@
-d = {
+dict1 = {
   'apple': ['malum', 'pomum', 'popula'],
   'fruit': ['baca', 'bacca', 'popum'],
   'punishment': ['malum', 'multa']
 }
+new_dict1 = {}
+
+for key, value in dict1.items():
+    for i in value:
+        if i not in new_dict1:
+            new_dict1[i] = [key]
+        else:
+            new_dict1[i].append(key)
+print(new_dict1)
