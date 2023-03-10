c4c1beb82929bc0e9349731dda90f3cf471b1cd3
Oleksandr <bala1983@ukr.net>
2023-03-10 20:49:57 +0200

    DZ20

--- DZ20.py
+++ DZ20.py
@@ -5,7 +5,7 @@ M = int(input('Введіть матрицю МхМ '))
 
 def sort_matrix(M):
     if M < 6:
-        raise ValueError("Введіть матрицю МхМ")
+        raise ValueError("Введіть матрицю МхМ ")
 
     random_numbers = [[random.randint(1, 50) for i in range(M)] for j in range(M)]
     summa_of_stolb = [sum(column) for column in zip(*random_numbers)]
