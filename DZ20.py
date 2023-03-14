50a3b5976c0a98aaa8a1c7cf1f9a4c4c8f264477
Oleksandr <bala1983@ukr.net>
2023-03-14 23:21:13 +0200

    DZ20

--- DZ20.py
+++ DZ20.py
@@ -1,39 +1,66 @@
 import random
 
-M = int(input("Введіть значення М: "))
+M = int(input('Zadai chsilo ryad i stolb '))
 
 
 def sort_matrix(M):
-    if M < 6:
-        raise ValueError("Введіть значення М ")
+    # if M < 6:
+    #     raise ValueError("Plohoe chislo")
 
-    matrix = [[random.randint(10, 50) for j in range(M)] for i in range(M)]
-    print("До сортування:")
-    for row in matrix:
-        print(row)
+    random_numbers = [[random.randint(1, 50) for i in range(M)] for j in range(M)]
+    summa_of_stolb = [sum(column) for column in zip(*random_numbers)]
 
-    column_sums = [0] * M
+
+    print("Ne sort matriza")
+    for row in random_numbers:
+        for element in row:
+            print("{:<4}".format(element), end="")
+        print("")
+    for q in summa_of_stolb:
+        print("{:<4}".format(q), end="")
+
+
+    summa_dict = {}
+    for j in range(M):
+        summa_dict[j] = summa_of_stolb[j]
+
+
+    sorted_summa_dict = {}
     for i in range(M):
-        for j in range(M):
-            column_sums[j] += matrix[i][j]
-    print(column_sums)
+        min_key = min(summa_dict, key=summa_dict.get)
+        sorted_summa_dict[min_key] = summa_dict.pop(min_key)
+
+
+    sorted_numbers = [[random_numbers[i][j] for j in sorted_summa_dict.keys()] for i in range(M)]
+
 
     for j in range(M):
         if j % 2 == 0:
-            column = [matrix[i][j] for i in range(M)]
-            column.sort()
-            for i in range(M):
-                matrix[i][j] = column[i]
+            for i in range(1, M):
+                key_item = sorted_numbers[i][j]
+                k = i - 1
+                while k >= 0 and sorted_numbers[k][j] < key_item:
+                    sorted_numbers[k + 1][j] = sorted_numbers[k][j]
+                    k -= 1
+                sorted_numbers[k + 1][j] = key_item
         else:
-            column = [matrix[i][j] for i in range(M)]
-            column.sort(reverse=True)
-            for i in range(M):
-                matrix[i][j] = column[i]
+            for i in range(1, M):
+                key_item = sorted_numbers[i][j]
+                k = i - 1
+                while k >= 0 and sorted_numbers[k][j] > key_item:
+                    sorted_numbers[k + 1][j] = sorted_numbers[k][j]
+                    k -= 1
+                sorted_numbers[k + 1][j] = key_item
 
-    print("Після сортування:")
-    for row in matrix:
-        print(row)
-    print(column_sums)
+
+
+    print("\nSort matriza")
+    for row in sorted_numbers:
+        for element in row:
+            print("{:<4}".format(element), end="")
+        print("")
+    for q in sorted_summa_dict.values():
+        print("{:<4}".format(q), end="")
 
 
 sort_matrix(M)
