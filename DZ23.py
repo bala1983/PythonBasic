6d72966131023722b5adfd623421e099177e2eb1
Oleksandr <bala1983@ukr.net>
2023-03-15 19:16:48 +0200

    DZ23

--- DZ23.py
+++ DZ23.py
@@ -1,7 +1,7 @@
 class Counter:
 
     def __init__(self, start, min_value, max_value):
-        self.start_value = start
+        self.current_value = start
         self.min = min_value
         self.max = max_value
 
@@ -12,19 +12,19 @@ class Counter:
         self.max = max_value
 
     def start(self, start):
-        self.start_value = start
+        self.current_value = start
 
     def increment(self):
-        if self.start_value < self.max:
-            self.start_value +=1
+        if self.current_value < self.max:
+            self.current_value +=1
         else:
             print("Max Value ", max)
 
-    def rotation(self):
-        return self.start_value
+    def current(self):
+        return self.current_value
 
 
 counter = Counter(start=0, min_value=0, max_value=10)
 counter.increment()
-value = counter.rotation()
+value = counter.current()
 print(value)
