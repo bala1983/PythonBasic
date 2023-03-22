3eed40d61d449b7df236b68bba091384a2e5c946
Oleksandr <bala1983@ukr.net>
2023-03-22 20:27:49 +0200

    DZ25

---
+++ DZ25.py
@@ -0,0 +1,26 @@
+class Buffer:
+    def __init__(self):
+        self.numbers = []
+
+    def add(self, *a):
+        self.numbers += list(a)
+        while len(self.numbers) >= 5:
+            print(sum(self.numbers[:5]))
+            self.numbers = self.numbers[5:]
+
+    def get_current_part(self):
+        return sum(self.numbers)
+
+
+if __name__ == "__main__":
+    buf = Buffer()
+    buf.add(5, 5, 6)
+    buf.get_current_part()
+    buf.add(1, 11, 5, 8, 6, 4)
+    buf.get_current_part()
+    buf.add(1, 2)
+    buf.get_current_part()
+    buf.add(1, 2, 3, 4)
+    buf.get_current_part()
+    buf.add(15, 24, 45, 89)
+    buf.get_current_part()
